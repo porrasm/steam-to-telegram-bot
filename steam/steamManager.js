ad79d71a54981453f98a4f3710896e0e4b218a60
Eetu Ikonen <eetu.e.ikonen@gmail.com>
2020-11-08 20:13:57 +0200

    Fix multiple login causing multiple subscriptions

--- steam/steamManager.js
+++ steam/steamManager.js
@@ -23,7 +23,6 @@ const login = (accountName, password, twoFactorCode) => {
 
     console.log("Call login: ", new Error().stack)
 
-    client = new User()
     const apiKey = process.env.STEAM_API_KEY
 
     logger.log("Logging in...", {
@@ -35,25 +34,6 @@ const login = (accountName, password, twoFactorCode) => {
     const loginOptions = {accountName, password, twoFactorCode}
     
     client.logOn(loginOptions)
-
-    client.on('loggedOn', function(details) {
-        logger.log("Logged into Steam as " + client.steamID.getSteam3RenderedID());
-        client.setPersona(settings.defaultSteamState);
-        chatBot.startChatBot()
-        telegramBot.sendBotMessage("Logged in on Steam as user: " + accountName)
-    });
-    
-    client.on('error', function(e) {
-        // Some error occurred during logon
-        logger.log("Error logging in", {
-            accountName,
-            password: "password",
-            twoFactorCode,
-            e
-        })
-        telegramBot.sendBotMessage("Error logging in on Steam as user: " + accountName)
-    });
-
     steamClient = client
 }
 
@@ -87,6 +67,29 @@ const getPersona = (steamID) => {
     })
 }
 //#endregion
+
+const init = () => {
+    
+    client.on('loggedOn', function(details) {
+        logger.log("Logged into Steam as " + client.steamID.getSteam3RenderedID());
+        client.setPersona(settings.defaultSteamState);
+        chatBot.startChatBot()
+        telegramBot.sendBotMessage("Logged in on Steam as user: " + accountName)
+    });
+    
+    client.on('error', function(e) {
+        // Some error occurred during logon
+        logger.log("Error logging in", {
+            accountName,
+            password: "password",
+            twoFactorCode,
+            e
+        })
+        telegramBot.sendBotMessage("Error logging in on Steam as user: " + accountName)
+    });
+}
+
+init()
 
 module.exports = {
     getClient,
